eeeeef5c5220da341d73626d6cde30cf5fcd8f46
nicolas-camacho <nicolas.camacho@ideaware.co>
2021-02-24 22:17:32 -0500

    :sparkles: Getting phrases by id

--- api/routes/phrases.go
+++ api/routes/phrases.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/nicolas-camacho/phrase_api/pkg/phrases"
 )
@@ -8,6 +10,7 @@ import (
 //PhrasesRouter is use to set the routes of the phrase service
 func PhrasesRouter(app fiber.Router, service phrases.PhraseService) {
 	app.Post("/phrases", addPhrase(service))
+	app.Get("/phrases/:id", getPhrase(service))
 }
 
 func addPhrase(service phrases.PhraseService) fiber.Handler {
@@ -18,8 +21,22 @@ func addPhrase(service phrases.PhraseService) fiber.Handler {
 		}
 		response, dberr := service.CreatePhrase(&requestBody)
 		if dberr != nil {
-			return context.Status(fiber.StatusInternalServerError).SendString("Error while inserting the phrase")
+			return context.Status(fiber.StatusInternalServerError).SendString(dberr.Error())
 		}
 		return context.Status(fiber.StatusCreated).JSON(response)
+	}
+}
+
+func getPhrase(service phrases.PhraseService) fiber.Handler {
+	return func(context *fiber.Ctx) error {
+		requestedID, err := strconv.Atoi(context.Params("id"))
+		if err != nil {
+			return context.Status(fiber.StatusBadRequest).SendString("Invalid Id")
+		}
+		response, dberr := service.ReadPhrase(requestedID)
+		if dberr != nil {
+			return context.Status(fiber.StatusInternalServerError).SendString(dberr.Error())
+		}
+		return context.Status(fiber.StatusOK).JSON(response)
 	}
 }
